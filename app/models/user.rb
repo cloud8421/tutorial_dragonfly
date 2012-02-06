1666d705cd647bd9d0fd5800f5a6ee85b31926f8
Claudio Ortolina <claudio@jumpzero.com>
2012-02-06 22:22:52 +0000

    Added name method to user
    Signed-off-by: Claudio Ortolina <claudio@jumpzero.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 	validates_presence_of :first_name
 	validates_presence_of :last_name
 
+	def name
+		[first_name, last_name].join ' '
+	end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,20 @@ describe User do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
 
+	end
+
+	context "attributes" do
+
+		let(:user) { Factory(:user, :first_name => "John", :last_name => "Doe") }
+
+		it "should have a name attribute" do
+			user.should respond_to(:name)
+		end
+
+		it "should have the right name" do
+			user.name.should eq('John Doe')
+		end
+
 	end  
 
 end
