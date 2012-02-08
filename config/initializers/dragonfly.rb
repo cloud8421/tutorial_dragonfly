80b0294b10cc220e398c0231b73dc4256fe052fd
Claudio Ortolina <claudio@jumpzero.com>
2012-02-08 11:06:39 +0000

    Added Dragonfly sample config for S3

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,1 +1,19 @@
-require 'dragonfly/rails/images'
+require 'dragonfly'
+
+app = Dragonfly[:images]
+app.configure_with(:imagemagick)
+app.configure_with(:rails)
+
+Dragonfly::App[:images].configure do |c|
+
+  c.datastore = Dragonfly::DataStorage::S3DataStore.new
+
+  c.datastore.configure do |d|
+    d.bucket_name = 'dragonfly_tutorial'
+    d.access_key_id = 'some_access_key_id'
+    d.secret_access_key = 'some_secret_access_key'
+  end
+
+end unless %(development test cucumber).include? Rails.env
+
+app.define_macro(ActiveRecord::Base, :image_accessor)
